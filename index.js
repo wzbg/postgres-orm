0f306815f522018fa91b3778ff37ed513fb08ea5
zyc <z35305515@gmail.com>
2016-01-15 22:52:57 +0800

    createTable

--- index.js
+++ index.js
@@ -2,17 +2,18 @@
 * @Author: zyc
 * @Date:   2016-01-15 14:32:12
 * @Last Modified by:   zyc
-* @Last Modified time: 2016-01-15 21:18:08
+* @Last Modified time: 2016-01-15 22:50:16
 */
 'use strict';
 
 const assert = require('assert');
+const S = require('string');
 
 const DBConnection = require('./connect');
 
 class EntityDB {
   static setup(conString) {
-    this.db = new DBConnection(conString);
+    return this.db = new DBConnection(conString);
   }
 
   static define(definition) {
@@ -42,6 +43,9 @@ class EntityDB {
   }
 
   set definition(definition) {
+    if (typeof definition  == 'string') {
+      definition = { name: definition };
+    }
     assert.ok(definition, 'definition required');
     assert.ok(definition.name, 'definition name required');
     this._definition = definition;
@@ -53,18 +57,33 @@ class EntityDB {
   }
 
   createTable() {
-    const dataTypes = ['id SERIAL'];
+    const dataTypes = [];
+    const pkey = this.definition.pkey;
+    if (pkey) {
+      const key = S(pkey.name).underscore();
+      dataTypes.push(`"${key}" ${pkey.type}`);
+      dataTypes.push(`CONSTRAINT ${this.name}_pkey PRIMARY KEY (${key})`);
+    }
     const attributes = this.definition.attributes;
     if (attributes) {
       for (let attr of attributes) {
-        let dataTyp = `"${attr.name}" ${attr.type}`;
-        if (attr.notNull) dataTyp += ` NOT NULL`;
-        if (attr.default) dataTyp += ` DEFAULT ${attr.default}`;
-        if (attr.unique) dataTyp += ` UNIQUE`;
+        let dataTyp = `"${S(attr.name).underscore()}" ${attr.type}`;
+        if (attr.notNull) {
+          dataTyp += ` NOT NULL`;
+        }
+        if (attr.default != undefined) {
+          dataTyp += ` DEFAULT '${attr.default}'`;
+        }
+        if (attr.unique) {
+          dataTyp += ` UNIQUE`;
+        }
         dataTypes.push(dataTyp);
       }
     }
-    dataTypes.push(`CONSTRAINT ${this.name}_pkey PRIMARY KEY (id)`);
+    if (this.definition.timestamps) {
+      dataTypes.push('created_at timestamp with time zone');
+      dataTypes.push('updated_at timestamp with time zone');
+    }
     return this.db.query(`CREATE TABLE "${this.name}"(${dataTypes.join(',')})`);
   }
 };
